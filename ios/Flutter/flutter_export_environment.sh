1bf02015863a501884a3f1663aa198513443ec2b
jayo2005 <info@dscomputers.ie>
2023-08-11 21:02:21 +0100

    Changed Odoo URLs to local IP address for development.

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter310\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Repos\Odoo-JsonRpc-with-Flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\SoftCroft\Odoo-JsonRpc-with-Flutter-master\Odoo-JsonRpc-with-Flutter-master"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
