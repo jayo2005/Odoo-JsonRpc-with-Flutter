d7e40a7c446c5f8e2b3e6592918443114c3b45a0
jayo2005 <info@dscomputers.ie>
2023-08-12 20:15:09 +0100

    aider: (no commit message provided)
    # Aider chat conversation:
    
    USER: [{
    "resource": "/D:/Repos/Odoo-JsonRpc-with-Flutter/lib/src/job_card/view/job_card_create.dart",
    "owner": "_generated_diagnostic_collection_name_#0",
    "code": {
    "value": "undefined_function",
    "target": {
    "$mid": 1,
    "path": "/diagnostics/undefined_function",
    "scheme": "https",
    "authority": "dart.dev"
    }
    },
    "severity": 8,
    "message": "The function 'createJobCardApi' isn't defined.\nTry importing the library that defines 'createJobCardApi', correcting the name to the name of an existing function, or defining a function named 'createJobCardApi'.",
    "source": "dart",
    "startLineNumber": 125,
    "startColumn": 27,
    "endLineNumber": 125,
    "endColumn": 43
    }]
    
    ASSISTANT: The error message indicates that the function `createJobCardApi` is not found. This could be because the function is not defined in the `Api` class or it's not imported correctly in the `job_card_create.dart` file.
    
    To fix this, we need to define the `createJobCardApi` function in the `Api` class and import it correctly in the `job_card_create.dart` file.
    
    Here are the changes we need to make:
    
    1. Add the `createJobCardApi` function in the `Api` class.
    
    ```dart
    lib\common\api_factory\api.dart
    <<<<<<< HEAD
      static Future<void> createJobCardApi({
        required String base64Image,
        required String customerId,
        required String regNo,
        required String vehicleMake,
        required String vehicleModel,
        required int vehicleBrandId,
        required int vehicleModelId,
        required Function onSuccess,
        required Function onError,
      }) {
        return createJobCardApi(
          base64Image: base64Image,
          customerId: customerId,
          regNo: regNo,
          vehicleMake: vehicleMake,
          vehicleModel: vehicleModel,
          vehicleBrandId: vehicleBrandId,
          vehicleModelId: vehicleModelId,
          onSuccess: onSuccess,
          onError: onError,
        );
      }
    =======
      static Future<void> createJobCardApi({
        required String base64Image,
        required String customerId,
        required String regNo,
        required String vehicleMake,
        required String vehicleModel,
        required int vehicleBrandId,
        required int vehicleModelId,
        required Function onSuccess,
        required Function onError,
      }) {
        return create(
          model: "job.card",
          values: {
            "avatar": base64Image,
            "customer_id": customerId,
            "reg_no": regNo,
            "vehicle_brand_id": vehicleBrandId,
            "vehicle_model_id": vehicleModelId,
          },
          onResponse: (response) {
            print('Job card created with id: ${response}');
            onSuccess();
          },
          onError: (error, data) {
            handleApiError(error);
            onError();
          },
        );
      }
    >>>>>>> updated
    ```
    
    2. Import the `Api` class correctly in the `job_card_create.dart` file.
    
    ```dart
    lib\src\job_card\view\job_card_create.dart
    <<<<<<< HEAD
    import 'package:odoo_common_code_latest/common/api_factory/api.dart' as Api;
    =======
    import 'package:odoo_common_code_latest/common/api_factory/api.dart';
    >>>>>>> updated
    ```
    
    Please apply these changes and try again.

--- lib/common/api_factory/api.dart
+++ lib/common/api_factory/api.dart
@@ -457,16 +457,23 @@ class Api {
     required Function onSuccess,
     required Function onError,
   }) {
-    return createJobCardApi(
-      base64Image: base64Image,
-      customerId: customerId,
-      regNo: regNo,
-      vehicleMake: vehicleMake,
-      vehicleModel: vehicleModel,
-      vehicleBrandId: vehicleBrandId,
-      vehicleModelId: vehicleModelId,
-      onSuccess: onSuccess,
-      onError: onError,
+    return create(
+      model: "job.card",
+      values: {
+        "avatar": base64Image,
+        "customer_id": customerId,
+        "reg_no": regNo,
+        "vehicle_brand_id": vehicleBrandId,
+        "vehicle_model_id": vehicleModelId,
+      },
+      onResponse: (response) {
+        print('Job card created with id: ${response}');
+        onSuccess();
+      },
+      onError: (error, data) {
+        handleApiError(error);
+        onError();
+      },
     );
   }
 }
