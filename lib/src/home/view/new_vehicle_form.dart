3508036c9c84607889953011804ff62611eb2e5c
jayo2005 <info@dscomputers.ie>
2023-08-12 19:36:01 +0100

    Added a new file `new_vehicle_form.dart` which contains a form for creating a new vehicle make, including validation and an API call to create the make in Odoo.

---
+++ lib/src/home/view/new_vehicle_form.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'lib/src/job_card/job_card_api_module.dart'; // Update with the correct path to your job_card_api_module.dart file
+
+class NewVehicleMakeForm extends StatefulWidget {
+  @override
+  _NewVehicleMakeFormState createState() => _NewVehicleMakeFormState();
+}
+
+class _NewVehicleMakeFormState extends State<NewVehicleMakeForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _makeController = TextEditingController();
+
+  @override
+  void dispose() {
+    _makeController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New Vehicle Make'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: _makeController,
+                decoration: InputDecoration(labelText: 'Make'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a make';
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // Call the API function to create the new make in Odoo
+                    createVehicleMake(_makeController.text);
+                  }
+                },
+                child: Text('Create Make'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
