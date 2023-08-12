2b16e90a1ae262e3b27c73d0283a1e5526bd6bc3
jayo2005 <info@dscomputers.ie>
2023-08-12 04:30:29 +0100

    aider: Removed unnecessary `http.get` calls for fetching vehicle makes and models.
    # Aider chat conversation:
    
    USER: wy are yu usig this i dont have amy 'https://your-api-url.com/vehicle-makes'
    
    ASSISTANT: I apologize for the confusion. The URLs 'https://your-api-url.com/vehicle-makes' and 'https://your-api-url.com/vehicle-models' were placeholders for the actual API endpoints where you fetch vehicle makes and models.
    
    Since you're using Odoo, you should replace these placeholders with the actual Odoo API endpoints for fetching vehicle makes and models.
    
    However, it seems like you're already fetching vehicle makes and models using the `Api.read` method in the `getVehicleMakes` and `getVehicleModels` functions. If that's the case, you don't need the `http.get` calls.
    
    Here are the *edit blocks* to remove the `http.get` calls:
    
    ```dart
    lib\common\api_factory\modules\job_card_api_module.dart
    <<<<<<< HEAD
    Future<List<String>> getVehicleMakes() async {
      // Call the Odoo API to fetch the vehicle makes
      // The actual implementation may vary depending on your Odoo server and API
      // If the API call fails or doesn't return any data, return an empty list
      var response = await http.get('https://your-api-url.com/vehicle-makes');
      if (response.statusCode == 200) {
        return jsonDecode(response.body);
      } else {
        return [];
      }
    }
    
    Future<List<String>> getVehicleModels() async {
      // Call the Odoo API to fetch the vehicle models
      // The actual implementation may vary depending on your Odoo server and API
      // If the API call fails or doesn't return any data, return an empty list
      var response = await http.get('https://your-api-url.com/vehicle-models');
      if (response.statusCode == 200) {
        return jsonDecode(response.body);
      } else {
        return [];
      }
    }
    =======
    >>>>>>> updated
    ```
    
    Please apply these changes to your `job_card_api_module.dart` file.

--- lib/common/api_factory/modules/job_card_api_module.dart
+++ lib/common/api_factory/modules/job_card_api_module.dart
@@ -35,29 +35,6 @@ createJobCardApi({
 }
 
 Future<List<String>> getVehicleMakes() async {
-  // Call the Odoo API to fetch the vehicle makes
-  // The actual implementation may vary depending on your Odoo server and API
-  // If the API call fails or doesn't return any data, return an empty list
-  var response = await http.get('https://your-api-url.com/vehicle-makes');
-  if (response.statusCode == 200) {
-    return jsonDecode(response.body);
-  } else {
-    return [];
-  }
-}
-
-Future<List<String>> getVehicleModels() async {
-  // Call the Odoo API to fetch the vehicle models
-  // The actual implementation may vary depending on your Odoo server and API
-  // If the API call fails or doesn't return any data, return an empty list
-  var response = await http.get('https://your-api-url.com/vehicle-models');
-  if (response.statusCode == 200) {
-    return jsonDecode(response.body);
-  } else {
-    return [];
-  }
-}
-Future<List<String>> getVehicleMakes() async {
   List<String> makes = [];
   Api.read(
     model: 'vehicle.make',
